fc095dcf15dedfa2c51697557ec11b832bf49d0d
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-20 15:27:39 +0530

    Update katex_basic.js

--- sketches/katex_basic.js
+++ sketches/katex_basic.js
@@ -2,10 +2,10 @@ let tex;
 function setup() {
   createCanvas(400, 400);                 
   tex = createP(); 
-  tex.style('font-size', '14px',);                   //font-size
-  tex.position(width/2, height/2);                   //position
+  tex.style('font-size', '14px',);                                                 //font-size
+  tex.position(width*1/5, height*2/5);                                             //position
   katex.render('\\Huge F = \\Huge G \\frac{m_{1}\\cdot m_{2}}{r^{2}}', tex.elt);   //KaTex script 
 }
 function draw() {  
-    background(135,206,235);  
+    background(255);  
 }
