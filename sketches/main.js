fd1dae70915795c5a9dea599d7985d6e74c8ce9f
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-20 11:18:18 +0530

    Update main.js

--- sketches/main.js
+++ sketches/main.js
@@ -4,9 +4,20 @@ function setup() {
   createCanvas(240, 240);
   input = createInput();
   input.position(20, 65);
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(scriptChange);
 }
 
 function draw() {
   background(255); 
 }
 
+function scriptChange() {
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = input;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+}
