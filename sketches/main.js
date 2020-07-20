abf7e8c7f424ff4c1b32bab96af48cb617d787d4
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-20 11:42:47 +0530

    Update main.js

--- sketches/main.js
+++ sketches/main.js
@@ -17,7 +17,7 @@ function scriptChange() {
     var po = document.createElement('script');
     po.type = 'text/javascript';
     po.async = true;
-    po.src = input;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
+    po.src = 'sketches/definite_integral.js';
+    //var s = document.getElementById('jsTarget')[0];
+    //s.parentNode.insertBefore(po, s);
 }
