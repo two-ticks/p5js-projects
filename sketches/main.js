c99f486352d93e15c2c9fc05d614e26a5b850d63
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-20 13:19:31 +0530

    Update main.js

--- sketches/main.js
+++ sketches/main.js
@@ -1,8 +1,22 @@
-(function() {
+let input;
+function setup() {
+  createCanvas(240, 240);
+  input = createInput();
+  input.position(20, 65);
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(scriptChange);
+}
+
+function draw() {
+  background(255); 
+}
+
+function scriptChange() {
     var po = document.createElement('script');
     po.type = 'text/javascript';
     po.async = true;
-    po.src = 'sketches/definite_integral.js';
-    //var s = document.getElementById('jsTarget')[0];
-    //s.parentNode.insertBefore(po, s);
-})();
+    po.src = input;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+}
