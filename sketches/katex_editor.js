4d0a7779ef69cbaf50fbec985d9f1476b5781359
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-25 15:33:11 +0530

    Adding drag feature

--- sketches/katex_editor.js
+++ sketches/katex_editor.js
@@ -2,27 +2,39 @@ let tex;
 let katexScript;
 let fontSize;
 let size;
+let grabbed=false;
 
 function setup() {
     createCanvas(400, 400); 
     fontSize = createSlider(3,50,14,0.1);
-    fontSize.position(0,height);
+    fontSize.position(width*2/3,height);
     katexScript = createInput('\\Huge F = \\Huge G \\frac{m_{1}\\cdot{m_{2}}}{r^{2}}');
-    katexScript.position(width*2.85/5,height);
+    katexScript.position(0,height);
+    katexScript.size(200,200);
     tex = createP();
-    tex.position(width/5, height/3);    
+    tex.position(width/5, height/3);
 }
 
 function draw() {  
     background(155);
     tex.style('font-size', fontSize.value()+'px');
     katex.render(katexScript.value(), tex.elt);
+    tex.mousePressed(clicked);
 }
 
+function clicked(){
+    grabbed=true;
+}
+
+function mouseReleased(){
+    grabbed=false;
+}
 function mouseDragged(){
-    if(mouseX<=width && mouseY<=height)
+    if((grabbed==true)&&(mouseX<=width && mouseY<=height))
     {
-       tex.position(mouseX, mouseY);
+       
+       tex.position(pmouseX, pmouseY);
+       
     }
     
 }
