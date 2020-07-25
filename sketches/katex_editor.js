bdb54c2507cdffc3a318b768c9be7efa9c3e8282
TwoTicks <68433541+two-ticks@users.noreply.github.com>
2020-07-25 15:38:59 +0530

    Update katex_editor.js

--- sketches/katex_editor.js
+++ sketches/katex_editor.js
@@ -10,7 +10,7 @@ function setup() {
     fontSize.position(width*2/3,height);
     katexScript = createInput('\\Huge F = \\Huge G \\frac{m_{1}\\cdot{m_{2}}}{r^{2}}');
     katexScript.position(0,height);
-    katexScript.size(200,200);
+    katexScript.size(200,100);
     tex = createP();
     tex.position(width/5, height/3);
 }
